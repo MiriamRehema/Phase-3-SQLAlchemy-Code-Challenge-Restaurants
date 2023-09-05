76ecdabd7e752e008694de12f0854321375af764
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-05 11:01:56 +0300

    data

---
+++ alembic/versions/51da6832013e_create_review_restaurant_and_customer_.py
@@ -0,0 +1,25 @@
+"""Create Review,Restaurant,and Customer tables
+
+Revision ID: 51da6832013e
+Revises: 
+Create Date: 2023-09-04 12:29:10.450187
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51da6832013e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    
+    pass
+
+
+def downgrade():
+    pass
